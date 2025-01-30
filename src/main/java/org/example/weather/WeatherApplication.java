8f66eec0eece56bdcb46148783177bd7d256912b
MarcosPalacios <fmpalacios@alea-soluciones.com>
2025-01-30 13:23:52 +0100

    Rename project

--- src/main/java/org/example/weather/WeatherApplication.java
+++ src/main/java/org/example/weather/WeatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CopilotDemoApplication {
+public class WeatherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CopilotDemoApplication.class, args);
+        SpringApplication.run(WeatherApplication.class, args);
     }
 
 }
